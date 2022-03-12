388093c4cae20f7c32400980ada7ccf299182b11
shawn <shawn@tradeserv.com>
2022-03-12 12:55:03 +0800

    [master] init

---
+++ replace-ads-core/src/main/java/com/replace/ads/ReplaceAdsApplication.java
@@ -0,0 +1,13 @@
+package com.replace.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplaceAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplaceAdsApplication.class, args);
+	}
+
+}
---
+++ replace-ads-core/src/test/java/com/replace/ads/ReplaceAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replace.ads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplaceAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
